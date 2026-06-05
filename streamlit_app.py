215bde12c1e0cbcb92d6911266b5795f2fc24206
Gonzalo Céspedes Cocozza <cocozzag070@gmail.com>
2026-06-05 15:49:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list: 
         ingredients_string += fruit_chosen + ' '
-
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+#st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
 
 
@@ -56,5 +58,3 @@ if ingredients_list:
 
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
